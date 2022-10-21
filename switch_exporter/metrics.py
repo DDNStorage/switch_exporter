90b849fdfb8ba9750033097136fc2ca790468af5
zamance <zamance@ddn.com>
2022-10-20 18:15:37 -0600

    added fix for dashes in metric names - added all metrics from 'show interfaces ethernet counters'

--- switch_exporter/metrics.py
+++ switch_exporter/metrics.py
@@ -1,8 +1,13 @@
 COUNTERS = {
     'packets', 'multicast packets', 'unicast packets', 'broadcast packets',
-    'bytes', 'error packets', 'discard packets', 'packets of 64 bytes', 'packets Jumbo'
+    'bytes', 'error packets', 'discard packets', 'packets of 64 bytes', 
+    'packets of 128-255 bytes', 'packets of 256-511 bytes', 'packets of 512-1023 bytes', 
+    'packets of 1024-1518 bytes', 'packets Jumbo', 'fcs errors', 'undersize packets', 
+    'oversize packets', 'pause packets', 'unknown control opcode', 'symbol errors',
+    'discard packets by storm control'
 }
 
-
 def name_to_metric(name: str) -> str:
-    return 'switch_port_' + name.replace(' ', '_') + '_total'
+    name = 'switch_port_' + name.replace(' ', '_') + '_total'
+    # prometheus does not like dashses in metric names
+    return name.replace('-','_')
